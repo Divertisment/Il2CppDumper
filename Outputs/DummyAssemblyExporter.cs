33725e70c74e64357b8b893b0b1e7d78af0db2a4
wlastas <wlastas@gmail.com>
2025-08-16 04:52:21 +0300

    Add project files.

---
+++ Outputs/DummyAssemblyExporter.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Il2CppDumper {
+    public static class DummyAssemblyExporter {
+        public static void Export(Il2CppExecutor il2CppExecutor, string outputDir, bool addToken) {
+            if (Directory.Exists(outputDir))
+                Directory.Delete(outputDir, true);//we need delete all older files here
+            Directory.CreateDirectory(outputDir);
+            Directory.SetCurrentDirectory(outputDir);
+            var dummy = new DummyAssemblyGenerator(il2CppExecutor, addToken);
+            foreach (var assembly in dummy.Assemblies) {
+                using var stream = new MemoryStream();
+                assembly.Write(stream);
+                File.WriteAllBytes(assembly.MainModule.Name, stream.ToArray());
+            }
+        }
+    }
+}
